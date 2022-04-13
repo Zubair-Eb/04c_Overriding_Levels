24cac26348576325b352c2689d420635c0310fce
Zubair <zubair.yousaf-eberl@gmx.de>
2022-04-13 14:37:15 +0200

    13.04.22

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,9 @@ public class App {
     public static void main(String[] args) {
     
         Player player = new Player();
+        int rounds = 11;
 
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < rounds; i++) {
             ausgabe(player.play());  
         }
 
